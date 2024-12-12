1ad21f42bee2535e6992cb8c52d1dbc868f18b5d
Bruno de Lima <pyrobit@gmail.com>
2024-12-11 22:59:36 -0300

    test: renamee all descriptions to use new pattern

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,20 +4,24 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://0.0.0.0:3000/api/v1/status");
-  expect(response.status).toBe(200);
+describe("GET /api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://0.0.0.0:3000/api/v1/status");
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
-  console.log(responseBody);
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
-  expect(responseBody.dependencies.database.max_connections).toEqual(100);
-  expect(responseBody.dependencies.database.open_connections).toEqual(1);
+      const responseBody = await response.json();
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.dependencies.database.version).toEqual("16.0");
+      expect(responseBody.dependencies.database.max_connections).toEqual(100);
+      expect(responseBody.dependencies.database.open_connections).toEqual(1);
 
-  expect(responseBody).not.toHaveProperty("password");
-  expect(responseBody).not.toHaveProperty("email");
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+      expect(responseBody).not.toHaveProperty("password");
+      expect(responseBody).not.toHaveProperty("email");
+    });
+  });
 });
